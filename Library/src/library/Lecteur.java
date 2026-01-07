5e8d97ff40dc4cc636a1e70266f9434d2ec0000a
Kykero <your_email@example.com>
2026-01-07 15:26:29 +0100

    add : Lecteur Class

--- Library/src/library/Lecteur.java
+++ Library/src/library/Lecteur.java
@@ -1,3 +1,21 @@
+package library;
+
 public class Lecteur {
+    protected String Nom;
+    protected String Email;
+    protected String Institut;
+    protected int MaxEmprunt;
+
+    public Lecteur(String nom, String email, String institut, int maxEmprunt) {
+        this.Nom = nom;
+        this.Email = email;
+        this.Institut = institut;
+        this.MaxEmprunt = maxEmprunt;
+    }
+
+    //Getteurs et Setters
+    public String getNom() { return Nom; }
+    public int getMaxEmprunt() { return MaxEmprunt; }
+    public void setMaxEmprunt(int Max) { this.MaxEmprunt = Max; }
 
 }
